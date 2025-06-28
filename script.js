ff4fc38839b709bd7c6628cd8034eebb4a6ac586
k-sasaking <42212008+k-sasaking@users.noreply.github.com>
2025-06-28 13:00:00 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ 与えられた配列の中から、指定された文字で始まる単語だけを返す
+ 関数 filterAndCapitalize(words, letter) を作ってください。
+*/
+
+const words = ["apple", "banana", "apricot", "blueberry", "avocado"];
+const letter = "a";
+
+function filterAndCapitalize(words, letter) {
+   
+
+}
+
+console.log(filterAndCapitalize(words, letter))
+// "apple",  "apricot", "avocado"]
